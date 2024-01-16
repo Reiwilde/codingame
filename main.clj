f7d52a8736fde083fa27a65344e31e4003427001
Alexis Morel <Reiwilde@users.noreply.github.com>
2024-01-16 22:00:00 +0100

    clojure - solution 2

--- main.clj
+++ main.clj
@@ -3,16 +3,5 @@
 
 (defn -main [& args]
   (while true
-
-    (def mountains-height
-      (loop [i 8 r []]
-        (if (> i 0)
-          (recur (dec i) (conj r (read)))
-          r)))
-
-    (println
-     (loop [i 1 r 0]
-       (if (< i (count mountains-height))
-         (recur (inc i)
-                (if (> (get mountains-height i) (get mountains-height r)) i r))
-         r)))))
+    (let [heights (repeatedly 8 read)]
+      (println (first (apply max-key second (map-indexed list heights)))))))
